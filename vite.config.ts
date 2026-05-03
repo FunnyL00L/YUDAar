bd6e6c6b0d6f379de9c9a1c322f0aa9a292c5356
FunnyL00L <143920474+FunnyL00L@users.noreply.github.com>
2026-05-03 18:33:27 +0800

    feat: Optimize build and update dependencies
    Updates dependencies including react-three-fiber, react-three-drei, and three.
    Configures Vite to use manual chunks for better code splitting, especially for `three` and `react` vendors.
    This aims to improve initial load times by reducing the size of main JavaScript bundles.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Menyesuaikan batas menjadi 1500kb
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'three';
+            if (id.includes('react')) return 'react-vendor';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
